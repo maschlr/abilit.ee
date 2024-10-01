0de38050aa3175e0dc6c0f67114d50f0dffa4c5a
Marten (msc) <msc@odoo.com>
2024-10-01 15:40:44 -0500

    fix header, fix about layout

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,15 +1,14 @@
 import { slugifyStr } from "@utils/slugify";
 
-
 export interface ProjectProps {
-   highlights: string[];
-   summary: string;
-   website: string;
-   pinned?: boolean;
-   name: string;
-   position: string;
-   startDate: string,
-   endDate?: string
+  highlights: string[];
+  summary: string;
+  website: string;
+  pinned?: boolean;
+  name: string;
+  position: string;
+  startDate: string;
+  endDate?: string;
 }
 
 export default function Project(projectProps: ProjectProps) {
@@ -25,12 +24,13 @@ export default function Project(projectProps: ProjectProps) {
         className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
       >
         <h2 {...headerProps}>{projectProps.name}</h2>
-         
       </a>
       <div>
         <p>{projectProps.summary}</p>
         <ul className="pl-6 pt-2 text-sm list-disc">
-            {projectProps.highlights.map((highlight, index) => <li key={index}>{highlight}</li>)}
+          {projectProps.highlights.map((highlight, index) => (
+            <li key={index}>{highlight}</li>
+          ))}
         </ul>
       </div>
     </div>
